61ec5a98f40549c11c20732943ba2f014089d63c
CodeSandbox <support@codesandbox.io>
2023-12-29 14:54:12 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [file, setFile] = useState();
+  const [query, setQuery] = useState();
+
+  const getBase64 = (file) =>
+    new Promise(function (resolve, reject) {
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject("Error: ", error);
+    });
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <input
+        type="file"
+        onChange={async (e) => {
+          const file = e.target.files[0];
+          const data = {
+            inlineData: { data: await getBase64(file), mimeType: file.type },
+          };
+          setFile(data);
+        }}
+      />
+      <textarea onChange={(e) => setQuery(e.target.value)} />
+      <button>Testing</button>
+      {file && <div>file</div>}
+      {query && <div>query</div>}
     </div>
   );
 }
