27c64a24429ed9c34b6960ee3b7957b4d1d90b2b
Asa Choi <asa.choi@gmail.com>
2023-12-30 07:36:02 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,34 +1,56 @@
 import "./styles.css";
 import { useState } from "react";
+import axios from "axios";
 
 export default function App() {
   const [file, setFile] = useState();
   const [query, setQuery] = useState();
+  const [response, setResponse] = useState();
 
-  const getBase64 = (file) =>
-    new Promise(function (resolve, reject) {
-      let reader = new FileReader();
+  const askLLM = async () => {
+    const req = { file, query };
+    console.log(req);
+    const response = await axios.post(
+      "https://slowhc.buildship.run/query",
+      req,
+    );
+
+    setResponse(response.data);
+  };
+
+  async function fileToGenerativePart(file) {
+    const base64EncodedDataPromise = new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result.split(",")[1]);
       reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result);
-      reader.onerror = (error) => reject("Error: ", error);
     });
+
+    return {
+      inlineData: { data: await base64EncodedDataPromise, mimeType: file.type },
+    };
+  }
+
+  async function updateFile(files) {
+    const file = files[0];
+    const data = await fileToGenerativePart(file);
+
+    setFile(data);
+    console.log(data);
+  }
 
   return (
     <div className="App">
-      <input
-        type="file"
-        onChange={async (e) => {
-          const file = e.target.files[0];
-          const data = {
-            inlineData: { data: await getBase64(file), mimeType: file.type },
-          };
-          setFile(data);
-        }}
-      />
+      <input type="file" onChange={(e) => updateFile(e.target.files)} />
+      {response}
       <textarea onChange={(e) => setQuery(e.target.value)} />
-      <button>Testing</button>
-      {file && <div>file</div>}
-      {query && <div>query</div>}
+      <button onClick={() => askLLM()}>Testing</button>
+      {file && (
+        <p>
+          <img
+            src={`data:${file.inlineData.mimeType};base64,${file.inlineData.data}`}
+          />{" "}
+        </p>
+      )}
     </div>
   );
 }
